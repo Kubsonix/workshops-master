909632e0444393ed6c4c1103fa207d5bcf557226
Kubsonix <kuba.sniec@gmail.com>
2015-07-14 14:25:43 +0200

    Fixed Application Controller && New Registration Form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,10 @@ class ApplicationController < ActionController::Base
   end
 
   protect_from_forgery with: :exception
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def configure_permitted_parameters
+     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :firstname, :lastname, :password, :password_confirmation) }
+  end
 end
